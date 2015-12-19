91ac13eadf0e8a0df19d81f52a11d763268a0293
Gabriel Pichot <gabriel.pichot@gmail.com>
2015-12-19 01:43:29 +0100

    Replaces quotes

--- travis.sh
+++ travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 mkdir testbuild && cd testbuild
-echo 'yeotest\nyeotest\nYeotest\nme\ny\n' | yo cpp-suite
-echo 'MyClass\n\n\nn\n' | yo cpp-suite:addclass
+echo "yeotest\nyeotest\nYeotest\nme\ny\n" | yo cpp-suite
+echo "MyClass\n\n\nn\n" | yo cpp-suite:addclass
 
