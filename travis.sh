a5b7a28cb5083d063481a6e889ca13276de4db9a
Gabriel Pichot <gabriel.pichot@gmail.com>
2015-12-20 16:32:04 +0100

    Adds expect script for interaction

--- travis.sh
+++ travis.sh
@@ -1,6 +1,28 @@
-#!/bin/bash
+#!/usr/bin/expect
 
-mkdir testbuild && cd testbuild
-echo "yeotest\nyeotest\nYeotest\nme\ny\n" | yo cpp-suite
-echo "MyClass\n\n\nn\n" | yo cpp-suite:addclass
+spawn yo cpp-suite
+expect "What is the name of the project?"
+send "yeotest\r"
+expect "What is the slug of the project?"
+send "yeotest\r"
+expect "Write a short description of your project:"
+send "A build test\r"
+expect "Who is the creator?"
+send "Travis\r"
+expect "Create a benchmarks directory ?"
+send "yes\r"
 
+expect eof
+
+
+spawn yo cpp-suite:addclass
+expect "Class name:"
+send "Foo\r"
+expect "Inherits from:"
+send "\r"
+expect "In folder (src/...):"
+send "\r"
+expect "Just generate header in source?"
+send "n"
+
+expect eof
